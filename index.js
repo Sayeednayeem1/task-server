1de27bc14e176e2a7189a9541be78722d6450737
Abu Sayeed <sayeed@pop-os.localdomain>
2022-12-28 14:49:42 +0600

    added .env and mongodb uri name password

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 
 // todo middleware's
 app.use(cors());
 app.use(express.json());
+
+// todo mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qk4n58g.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 
 
@@ -14,6 +27,6 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Task server is running");
 });
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("Task server is running on port: ", port);
 });
